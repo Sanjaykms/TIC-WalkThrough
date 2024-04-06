b8762b91fba216f10bea571cb912fed83ddd9e00
Sanjaykms <48528626+Sanjaykms@users.noreply.github.com>
2024-04-06 17:37:57 +0530

    Adding the Files
    Committing the changes

---
+++ Assets/Sc.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Sc : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public VideoPlayer tv;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Avatar")
+        {
+            tv.Play();
+
+        }
+       
+    }
+   
+}
+
---
+++ Assets/TIC/Scenes/TrggerZone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrggerZone : MonoBehaviour
+{
+    public GameObject Video;
+    public GameObject CanVas;
+    void Start() {
+          Video.GetComponent<UnityEngine.Video.VideoPlayer>().Pause();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag== "Player")
+        {
+            Video.GetComponent<UnityEngine.Video.VideoPlayer>().Play();
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Video.GetComponent<UnityEngine.Video.VideoPlayer>().Pause();
+        }
+    }
+}
